c9e0441b9ea8639f14b26dd019c838e7dd391427
Modyzo <44831870+modyzo@users.noreply.github.com>
2022-03-20 10:35:25 +0200

    update

--- blog/core/models.py
+++ blog/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(blank=True, null=True)
+    phone = models.CharField(max_length=11)
+    address = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
--- blog/core/views.py
+++ blog/core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import CreateView, UpdateView, DetailView, DeleteView, ListView
 
 # functional view
-
-
 def home(request):
     # use render function to load html files
     return render(request, 'index.html')
+
+# class based views
+
+
+
